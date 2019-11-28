21fc32f25271b0877af0a97138a756545959040a
ThiagoFTelles <atendimento.telles@gmail.com>
2019-11-27 22:10:32 -0300

    aula 108

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'input_page.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0XFF10163B), //cor do header bottom e outras coisas
+        scaffoldBackgroundColor: Color(0XFF0D1134), //cor do background
+      ),
+      home: InputPage(),
+    );
+  }
+}
